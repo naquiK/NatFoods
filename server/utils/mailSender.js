f26fa8f2a7c24f34d99a9f42de4c1eb4f2a8c308
naquik <nknaqui72@gmail.com>
2025-10-11 19:56:29 +0530

    fix send mail

--- server/utils/mailSender.js
+++ server/utils/mailSender.js
@@ -2,7 +2,7 @@ import { Resend } from "resend"
 
 const resend = new Resend(process.env.RESEND_API_KEY)
 
-export const sendEmail = async (to, subject, html) => {
+export const mailSender = async (to, subject, html) => {
   try {
     const data = await resend.emails.send({
       from: "NatFoods <onboarding@resend.dev>", // or a verified domain email
